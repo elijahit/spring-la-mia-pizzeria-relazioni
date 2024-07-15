042a584569b11ce50815a9a339c92e8c9498d9bb
Elijah <64620855+elijahit@users.noreply.github.com>
2024-07-15 21:08:58 +0200

    ADD: Bottone e controller di delete

--- src/main/java/it/elijah/pizzeria/controller/IngredientController.java
+++ src/main/java/it/elijah/pizzeria/controller/IngredientController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import it.elijah.pizzeria.model.Ingredient;
-import it.elijah.pizzeria.model.Pizza;
 import it.elijah.pizzeria.repository.IngredientRepository;
 import jakarta.validation.Valid;
 
