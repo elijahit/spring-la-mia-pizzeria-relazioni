87a84ecb07cc6f3466b957c1148369bc0ff2572e
Elijah <64620855+elijahit@users.noreply.github.com>
2024-07-15 21:06:35 +0200

    ADD: Bottone e controller di delete

--- src/main/java/it/elijah/pizzeria/controller/IngredientController.java
+++ src/main/java/it/elijah/pizzeria/controller/IngredientController.java
@@ -7,14 +7,15 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import it.elijah.pizzeria.model.Ingredient;
+import it.elijah.pizzeria.model.Pizza;
 import it.elijah.pizzeria.repository.IngredientRepository;
 import jakarta.validation.Valid;
 
 import org.springframework.web.bind.annotation.PostMapping;
-
 
 @Controller
 @RequestMapping("/ingredients")
@@ -31,30 +32,38 @@ public class IngredientController {
 
     return "/ingredients/index";
 
-  }  
+  }
 
   @PostMapping("/create")
-  public String postMethodName(@Valid @ModelAttribute("ingrediente") Ingredient ingrediente, BindingResult bindingResult, Model model) {
-      
-      if(ingrediente.getIngredient() != null) {
-        Ingredient ingredienteFiltrato = ingredientRepository.findByIngredientIgnoreCase(ingrediente.getIngredient());
+  public String postMethodName(@Valid @ModelAttribute("ingrediente") Ingredient ingrediente,
+      BindingResult bindingResult, Model model) {
 
-        if(ingredienteFiltrato != null) {
-          bindingResult.addError(new ObjectError("Errore di inserimento", "E' già presente un ingrediente con questo nome"));
-        }
+    if (ingrediente.getIngredient() != null) {
+      Ingredient ingredienteFiltrato = ingredientRepository.findByIngredientIgnoreCase(ingrediente.getIngredient());
 
-      }
-      
-      if(bindingResult.hasErrors()) {
-        model.addAttribute("listaIngredienti", ingredientRepository.findAll());
-        model.addAttribute("ingrediente", ingrediente);
-        return "/ingredients/index";
+      if (ingredienteFiltrato != null) {
+        bindingResult
+            .addError(new ObjectError("Errore di inserimento", "E' già presente un ingrediente con questo nome"));
       }
 
-      
-      ingredientRepository.save(ingrediente);
-      return "redirect:/ingredients";
+    }
+
+    if (bindingResult.hasErrors()) {
+      model.addAttribute("listaIngredienti", ingredientRepository.findAll());
+      model.addAttribute("ingrediente", ingrediente);
+      return "/ingredients/index";
+    }
+
+    ingredientRepository.save(ingrediente);
+    return "redirect:/ingredients";
   }
-  
+
+  @PostMapping("/delete/{id}")
+  public String delete(@PathVariable("id") Integer id) {
+
+    ingredientRepository.delete(ingredientRepository.getReferenceById(id));
+
+    return "redirect:/ingredients";
+  }
 
 }
